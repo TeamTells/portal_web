851ccd4926f4c91a54d73e58e9156e858d77e75c
alexey.minay <aminay@alfabank.ru>
2023-10-12 23:48:12 +0300

    [TTP-29] Добавил фото

--- src/app/features/editor/presentation/editor/editor.component.ts
+++ src/app/features/editor/presentation/editor/editor.component.ts
@@ -11,173 +11,193 @@ import {StyleUtils} from "../../domain/html/style-utils";
 import {Span} from "../../domain/html/span";
 import {MenuCallbackImpl} from "./menu-callback-impl";
 import {Paragraph} from "../../domain/html/paragraph";
+import {Image} from "../../domain/html/image";
 
 @Component({
-    selector: 'app-editor',
-    templateUrl: './editor.component.html',
-    styleUrls: ['./editor.component.scss']
+  selector: 'app-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.scss']
 })
 export class EditorComponent extends Store<EditorState, EditorExecutor, EditorAction, EditorResultAction> implements AfterViewInit {
 
-    private dropDown: HTMLElement | null = null
-    private selectedElement: HTMLElement | null = null
-    private menu: HTMLElement | null = DropDown.create(new MenuCallbackImpl(() => {
-        return this.selectedElement
-    }))
+  private dropDown: HTMLElement | null = null
+  private selectedElement: HTMLElement | null = null
+  private menu: HTMLElement | null = DropDown.create(new MenuCallbackImpl(() => {
+    return this.selectedElement
+  }))
 
-    private menuButtonClickListener = (e: MouseEvent) => {
-        if (this.dropDown?.style.visibility == "hidden") {
-            this.dropDown.style.visibility = "visible"
-        } else if (this.dropDown?.style.visibility == "visible") {
-            this.dropDown.style.visibility = "hidden"
+  private menuButtonClickListener = (e: MouseEvent) => {
+    if (this.dropDown?.style.visibility == "hidden") {
+      this.dropDown.style.visibility = "visible"
+    } else if (this.dropDown?.style.visibility == "visible") {
+      this.dropDown.style.visibility = "hidden"
+    }
+    e.stopPropagation()
+  }
+
+  private documentClickListener = () => {
+    if (this.dropDown?.style.visibility == "visible") {
+      this.dropDown!.style.visibility = "hidden"
+    }
+  }
+
+  private keyShowMenuClickListener = (event: KeyboardEvent) => {
+    if (event.key == 'Enter') {
+      this.setMenuButton()
+    }
+  }
+
+  private menuClickListener = () => {
+    this.setMenuButton()
+  }
+
+  constructor(
+    state: EditorState,
+    executor: EditorExecutor,
+    reducer: EditorReducer,
+  ) {
+    super(state, executor, reducer);
+    this.subscribeToUpdateHtml()
+  }
+
+  ngAfterViewInit(): void {
+    document?.addEventListener('keydown', (event: KeyboardEvent) => {
+      if (event.key == 'Backspace' || event.key == 'Delete') {
+        const selection = document.getSelection()
+        const anchorNode = selection?.anchorNode
+
+        if (anchorNode?.parentElement?.parentElement?.getAttribute("paragraph-type") == "text") {
+          const parent = anchorNode?.parentElement?.parentElement
+          if (parent?.children?.length == 1 && parent?.children[0].children.length == 0) {
+            parent?.append(Span.create())
+            return;
+          }
+        } else if (anchorNode?.parentElement?.getAttribute("paragraph-type") == "text") {
+          const parent = anchorNode?.parentElement
+          if (parent?.children?.length == 1 && parent.children[0].innerHTML == '<br>') {
+            parent?.parentElement?.removeChild(parent)
+            return;
+          }
         }
-        e.stopPropagation()
-    }
+      }
 
-    private documentClickListener = () => {
-        if (this.dropDown?.style.visibility == "visible") {
-            this.dropDown!.style.visibility = "hidden"
-        }
-    }
+      if (event.key == 'Enter') {
+        const selection = document.getSelection()
+        const anchorNode = selection?.anchorNode
+        const isTitle = anchorNode?.parentElement?.id == "ed-title" ||
+          (document.getSelection()?.anchorNode as HTMLElement).id == "ed-title"
 
-    private keyShowMenuClickListener = (event: KeyboardEvent) => {
-        if (event.key == 'Enter') {
-            this.setMenuButton()
-        }
-    }
-
-    private menuClickListener = () => {
-        this.setMenuButton()
-    }
-
-    constructor(
-        state: EditorState,
-        executor: EditorExecutor,
-        reducer: EditorReducer,
-    ) {
-        super(state, executor, reducer);
-        this.subscribeToUpdateHtml()
-    }
-
-    ngAfterViewInit(): void {
-        document?.addEventListener('keydown', (event: KeyboardEvent) => {
-            if (event.key == 'Backspace' || event.key == 'Delete') {
-                const selection = document.getSelection()
-                const anchorNode = selection?.anchorNode
-
-                if (anchorNode?.parentElement?.parentElement?.getAttribute("paragraph-type") == "text") {
-                    const parent = anchorNode?.parentElement?.parentElement
-                    if (parent?.children?.length == 1 && parent?.children[0].children.length == 0) {
-                        parent?.append(Span.create())
-                        return;
-                    }
-                } else if (anchorNode?.parentElement?.getAttribute("paragraph-type") == "text") {
-                    const parent = anchorNode?.parentElement
-                    if (parent?.children?.length == 1 && parent.children[0].innerHTML == '<br>') {
-                        parent?.parentElement?.removeChild(parent)
-                        return;
-                    }
-                }
-            }
-
-            if (event.key == 'Enter') {
-                const selection = document.getSelection()
-                const anchorNode = selection?.anchorNode
-                const isTitle = anchorNode?.parentElement?.id == "ed-title" ||
-                    (document.getSelection()?.anchorNode as HTMLElement).id == "ed-title"
-
-                if (isTitle) {
-                    event.preventDefault()
-                    return
-                }
-
-                const isTextSpan = anchorNode?.parentElement?.id == "ed-text-span" ||
-                    (anchorNode?.parentElement?.firstChild as HTMLElement).id == "ed-text-span"
-
-                if (isTextSpan) {
-                    if (anchorNode?.parentElement?.parentElement?.getAttribute("paragraph-type") == "text") {
-                        const paragraph = anchorNode?.parentElement?.parentElement
-                        this.addTextParagraph(paragraph, selection)
-                    } else if (anchorNode?.parentElement?.getAttribute("paragraph-type") == "text") {
-                        const paragraph = anchorNode?.parentElement
-                        this.addTextParagraph(paragraph, selection)
-                    }
-
-                    event.preventDefault()
-                    return;
-                }
-            }
-        })
-    }
-
-    private addTextParagraph(element: HTMLElement | null, selection: Selection | null) {
-        const paragraph = Paragraph.create(ParagraphTypeConsts.text)
-        const span = Span.create()
-        span.setAttribute("style", "outline:none")
-        paragraph?.appendChild(span)
-
-        if (paragraph != null) {
-            element?.parentElement?.insertBefore(paragraph, element.nextSibling)
+        if (isTitle) {
+          event.preventDefault()
+          return
         }
 
-        const range = document.createRange()
-        range.selectNodeContents(span)
-        range.collapse(false)
-        selection?.removeAllRanges()
-        selection?.addRange(range)
-    }
+        const isTextSpan = anchorNode?.parentElement?.id == "ed-text-span" ||
+          (anchorNode?.parentElement?.firstChild as HTMLElement).id == "ed-text-span"
 
-    private setMenuButton() {
-        const element = document.getSelection()?.anchorNode?.parentElement
-        const paragraphType = element?.getAttribute("paragraph-type")
-        if (this.menu != null && paragraphType == ParagraphTypeConsts.text && element?.children.length == 1 && element?.children[0].textContent == "") {
-            this.selectedElement = element
+        if (isTextSpan) {
+          if (anchorNode?.parentElement?.parentElement?.getAttribute("paragraph-type") == "text") {
+            const paragraph = anchorNode?.parentElement?.parentElement
+            this.addTextParagraph(paragraph, selection)
+          } else if (anchorNode?.parentElement?.getAttribute("paragraph-type") == "text") {
+            const paragraph = anchorNode?.parentElement
+            this.addTextParagraph(paragraph, selection)
+          }
 
-            element?.parentElement?.insertBefore(this.menu, element)
+          event.preventDefault()
+          return;
         }
+      }
+    })
+  }
+
+  onImageFileSelect(event: any) {
+      console.log(event.target.files[0])
+      const imageFile = event.target.files[0]
+      const reader = new FileReader()
+      reader.addEventListener('load', (event) => {
+        console.log(event.target?.result)
+
+          const selectedElement = this.selectedElement
+          if (selectedElement == null) return
+
+          const div = Paragraph.create(ParagraphTypeConsts.image)
+          div.append(Image.create(event.target?.result?.toString()!))
+          selectedElement.parentElement?.insertBefore(div, selectedElement)
+          const index = Array.prototype.indexOf.call(selectedElement.parentNode?.childNodes, selectedElement)
+          selectedElement.parentElement?.insertBefore(div, selectedElement.parentElement.children[index - 2])
+      })
+      reader.readAsDataURL(imageFile)
+  }
+
+  private addTextParagraph(element: HTMLElement | null, selection: Selection | null) {
+    const paragraph = Paragraph.create(ParagraphTypeConsts.text)
+    const span = Span.create()
+    span.setAttribute("style", "outline:none")
+    paragraph?.appendChild(span)
+
+    if (paragraph != null) {
+      element?.parentElement?.insertBefore(paragraph, element.nextSibling)
     }
 
-    private subscribeToUpdateHtml() {
-        const div = document.querySelector('div');
-        const self = this
-        const divMO = new window.MutationObserver(function (e: MutationRecord[]) {
-            self.initDropdownMenu()
-            self.initTitle()
-        });
+    const range = document.createRange()
+    range.selectNodeContents(span)
+    range.collapse(false)
+    selection?.removeAllRanges()
+    selection?.addRange(range)
+  }
 
-        if (div != null) {
-            divMO.observe(div, {childList: true, subtree: true, characterData: true});
-        }
+  private setMenuButton() {
+    const element = document.getSelection()?.anchorNode?.parentElement
+    const paragraphType = element?.getAttribute("paragraph-type")
+    if (this.menu != null && paragraphType == ParagraphTypeConsts.text && element?.children.length == 1 && element?.children[0].textContent == "") {
+      this.selectedElement = element
+
+      element?.parentElement?.insertBefore(this.menu, element)
     }
+  }
 
-    private initTitle() {
-        const title = document.getElementById("ed-title")
-        if (title != null && title.innerHTML == "<br>") {
-            title.innerHTML = ""
-        }
+  private subscribeToUpdateHtml() {
+    const div = document.querySelector('div');
+    const self = this
+    const divMO = new window.MutationObserver(function (e: MutationRecord[]) {
+      self.initDropdownMenu()
+      self.initTitle()
+    });
+
+    if (div != null) {
+      divMO.observe(div, {childList: true, subtree: true, characterData: true});
     }
+  }
 
-    private initDropdownMenu() {
-        this.dropDown = document.getElementById("ed-drop-down")
-
-        const parent = document.getElementById("parent")
-
-        parent?.removeEventListener('click', this.menuClickListener)
-        parent?.addEventListener('click', this.menuClickListener)
-
-        const button = document.getElementById("ed-menu-button")
-
-        button?.removeEventListener('click', this.menuButtonClickListener)
-        button?.addEventListener('click', this.menuButtonClickListener)
-
-        document.removeEventListener('click', this.documentClickListener)
-        document.addEventListener('click', this.documentClickListener)
-
-        document?.removeEventListener('keyup', this.keyShowMenuClickListener)
-        document?.addEventListener('keyup', this.keyShowMenuClickListener)
+  private initTitle() {
+    const title = document.getElementById("ed-title")
+    if (title != null && title.innerHTML == "<br>") {
+      title.innerHTML = ""
     }
+  }
 
-    protected readonly TextParagraph = TextParagraph;
-    protected readonly StyleParser = StyleUtils;
-    protected readonly ImageParagraph = ImageParagraph;
+  private initDropdownMenu() {
+    this.dropDown = document.getElementById("ed-drop-down")
+
+    const parent = document.getElementById("parent")
+
+    parent?.removeEventListener('click', this.menuClickListener)
+    parent?.addEventListener('click', this.menuClickListener)
+
+    const button = document.getElementById("ed-menu-button")
+
+    button?.removeEventListener('click', this.menuButtonClickListener)
+    button?.addEventListener('click', this.menuButtonClickListener)
+
+    document.removeEventListener('click', this.documentClickListener)
+    document.addEventListener('click', this.documentClickListener)
+
+    document?.removeEventListener('keyup', this.keyShowMenuClickListener)
+    document?.addEventListener('keyup', this.keyShowMenuClickListener)
+  }
+
+  protected readonly TextParagraph = TextParagraph;
+  protected readonly StyleParser = StyleUtils;
+  protected readonly ImageParagraph = ImageParagraph;
 }
--- src/app/features/editor/presentation/editor/menu-callback-impl.ts
+++ src/app/features/editor/presentation/editor/menu-callback-impl.ts
@@ -4,34 +4,27 @@ import {Paragraph} from "../../domain/html/paragraph";
 import {ParagraphTypeConsts} from "../../domain/models/models";
 import {Image} from "../../domain/html/image";
 
-export class MenuCallbackImpl implements MenuCallbacks{
+export class MenuCallbackImpl implements MenuCallbacks {
 
-    constructor(private getSelectedElement: () => HTMLElement | null) {
-    }
+  constructor(private getSelectedElement: () => HTMLElement | null) {
+  }
 
-    onH1Click(): void {
-        this.getSelectedElement()?.childNodes?.forEach((element) => {
-            (element as HTMLElement)?.setAttribute("class", StyleUtils.h1())
-        })
-    }
+  onH1Click(): void {
+    this.getSelectedElement()?.childNodes?.forEach((element) => {
+      (element as HTMLElement)?.setAttribute("class", StyleUtils.h1())
+    })
+  }
 
 
-    onTextClick(): void {
-        this.getSelectedElement()?.children?.item(0)?.setAttribute("class", "")
-        this.getSelectedElement()?.childNodes?.forEach((element) => {
-            (element as HTMLElement)?.setAttribute("class", "")
-        })
-    }
+  onTextClick(): void {
+    this.getSelectedElement()?.children?.item(0)?.setAttribute("class", "")
+    this.getSelectedElement()?.childNodes?.forEach((element) => {
+      (element as HTMLElement)?.setAttribute("class", "")
+    })
+  }
 
-    onAddImage() {
-        const selectedElement = this.getSelectedElement()
-        if (selectedElement == null) return
-
-        const div = Paragraph.create(ParagraphTypeConsts.image)
-        div.append(Image.create("https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"))
-        selectedElement.parentElement?.insertBefore(div, selectedElement)
-        const index = Array.prototype.indexOf.call(selectedElement.parentNode?.childNodes, selectedElement)
-        selectedElement.parentElement?.insertBefore(div, selectedElement.parentElement.children[index - 2])
-    }
+  onAddImage() {
+    document.getElementById("ed-file-input")?.click()
+  }
 
 }
