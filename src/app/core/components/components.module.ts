11b5acf81cef9060be8b8931d089edb39c0a961a
Firdavsi <firdavsinurov326@gmail.com>
2023-10-12 16:43:19 +0300

    Add search field

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { SearchFieldComponent } from './search-field/search-field.component';
 
 @NgModule({
   declarations: [
     CardComponent,
+    SearchFieldComponent,
   ],
   exports: [
     CardComponent
