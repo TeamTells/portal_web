56ae89c46fe4ca584794dfddbb8eb23a466db038
Zhadi <zhadi000s@gmail.com>
2023-10-15 01:35:31 +0600

    .

--- src/app/features/section/presentation/section.component.ts
+++ src/app/features/section/presentation/section.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pages } from './pages';
 
 @Component({
   selector: 'app-section',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./section.component.scss']
 })
 export class SectionComponent {
+
+    pageItem: Pages[] = [
+        new Pages(1,'Паттерны проектирование'),
+        new Pages(2,'Строитель'),
+        new Pages(3,'Наблюдатель'),
+        new Pages(4,'Основы проиграммирование'),
+        new Pages(5, 'Основы веб разработки'),
+    ]
 
 }
