c15644111e90943f3955d499a39844106b3f5f67
alexey.minay <aminay@alfabank.ru>
2023-11-03 14:19:05 +0300

    [TTP-15] Добавил обновление статусов загрузки

--- src/app/features/authorization/presentation/state/authorization-reducer.ts
+++ src/app/features/authorization/presentation/state/authorization-reducer.ts
@@ -5,21 +5,27 @@ import {Injectable} from "@angular/core";
 import {clone} from "cloneable-ts";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AuthorizationReducer implements Reducer<AuthorizationState, AuthorizationResultAction> {
 
-    reduce(state: AuthorizationState, action: AuthorizationResultAction): AuthorizationState {
-        switch (action.type) {
-            case AuthorizationResultActionTypes.CHANGE_EMAIL:
-                return clone(state, {email: action.email, emailError: ""})
+  reduce(state: AuthorizationState, action: AuthorizationResultAction): AuthorizationState {
+    switch (action.type) {
+      case AuthorizationResultActionTypes.CHANGE_EMAIL:
+        return clone(state, {email: action.email, emailError: ""})
 
-            case AuthorizationResultActionTypes.CHANGE_PASSWORD:
-                return clone(state, {password: action.password, passwordError: ""})
+      case AuthorizationResultActionTypes.CHANGE_PASSWORD:
+        return clone(state, {password: action.password, passwordError: ""})
 
-            case AuthorizationResultActionTypes.VALIDATION_ERROR:
-                return clone(state, {emailError: action.emailError, passwordError: action.passwordError})
-        }
+      case AuthorizationResultActionTypes.VALIDATION_ERROR:
+        return clone(state, {emailError: action.emailError, passwordError: action.passwordError})
+
+      case AuthorizationResultActionTypes.CHANGE_LOADING_STATE:
+        return clone(state, {isLoading: !state.isLoading})
+
+      case AuthorizationResultActionTypes.UPDATE_LOGIN_ERROR_STATE:
+        return clone(state, {loginError: action.errorState})
     }
+  }
 
 }
