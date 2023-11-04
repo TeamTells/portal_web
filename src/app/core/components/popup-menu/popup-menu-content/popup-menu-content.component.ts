5551d702719c0996d75bfa27f9c7eb9ab8896d62
Alchimis <wafatyttest@gmail.com>
2023-11-04 18:00:02 +0300

    Поправил OnDestroy в PopupMenuComponent

--- src/app/core/components/popup-menu/popup-menu-content/popup-menu-content.component.ts
+++ src/app/core/components/popup-menu/popup-menu-content/popup-menu-content.component.ts
@@ -12,3 +12,35 @@ export class PopupMenuContentComponent{
   topOffset: string = "0px";
 
 }
+/*
+function updateBlockPosition() {
+  const block = document.getElementById('myBlock');
+  const blockRect = block.getBoundingClientRect();
+  const windowHeight = window.innerHeight;
+
+  // Проверяем, если нижняя граница блока находится за пределами видимой области
+  if (blockRect.bottom > windowHeight) {
+    const newTop = windowHeight - blockRect.height;
+    block.style.top = `${newTop}px`;
+  } else {
+    // Возвращаем блок в исходное положение
+    block.style.top = 'auto';
+  }
+}
+
+// Вызываем функцию при загрузке страницы и при изменении размера окна
+window.addEventListener('load', updateBlockPosition);
+window.addEventListener('resize', updateBlockPosition);
+
+
+
+ @HostListener('window:load', ['$event'])
+  onLoad(event: Event) {
+    // Обработка события загрузки окна
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    // Обработка события изменения размера окна
+  }
+*/
