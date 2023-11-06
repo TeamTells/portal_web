0fbd7c89cfd91c3af48d860a8a806dacb25f4f8a
alexey.minay <aminay@alfabank.ru>
2023-11-07 00:38:57 +0300

    [TTP-43] исправил верстку

--- src/app/features/sections/sections.module.ts
+++ src/app/features/sections/sections.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SectionsComponent } from './presentation/sections.component';
 import {FakeSectionService} from "./data/fake-section-service";
 import {SectionRepository} from "./domain/section-repository";
+import {ComponentsModule} from "../../core/components/components.module";
 
 @NgModule({
   declarations: [
     SectionsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule
   ],
   providers:[
     {
