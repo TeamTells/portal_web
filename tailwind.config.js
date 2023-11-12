a92ffb41e9f9f3e24f721029fe5a6ad7edad39de
AlexandrProhorov <efimpetr02@gmail.com>
2023-11-12 11:09:14 +0300

    sidebar done

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,8 @@ module.exports = {
         "tt-light-gray": "#9B9EA1",
         "tt-bg-error": "#FFF5F5",
         "tt-stroke-error": "#FFD8D8",
-        "tt-blue": "#007BFF"
+        "tt-blue": "#007BFF",
+        "tt-lighter-gray": "#BCC1C6"
       },
       maxWidth: {
         "tt-field": "334px",
