bb69b971f09243fac670e7739748694cec230b92
alexey.minay <aminay@alfabank.ru>
2023-12-06 23:31:48 +0300

    [TTP-60] Добавил создание рутового документа

--- src/app/features/section/impl/presentation/state/section-action.ts
+++ src/app/features/section/impl/presentation/state/section-action.ts
@@ -1,9 +1,11 @@
 export type SectionAction = ChangeDocumentOpenStateAction
   | ChangeContentOpenStateAction
+  | CreateDocumentAction
 
 export enum SectionActionTypes {
   CHANGE_DOCUMENT_OPEN_STATE,
-  CHANGE_CONTENT_OPEN_STATE
+  CHANGE_CONTENT_OPEN_STATE,
+  CREATE_DOCUMENT
 }
 
 export interface ChangeDocumentOpenStateAction {
@@ -13,4 +15,9 @@ export interface ChangeDocumentOpenStateAction {
 
 export interface ChangeContentOpenStateAction {
   readonly type: SectionActionTypes.CHANGE_CONTENT_OPEN_STATE
+}
+
+export interface CreateDocumentAction {
+  readonly type: SectionActionTypes.CREATE_DOCUMENT
+  readonly rootDocumentId: number
 }
--- src/app/features/section/impl/presentation/state/section-state.ts
+++ src/app/features/section/impl/presentation/state/section-state.ts
@@ -1,16 +1,15 @@
 import {Injectable} from "@angular/core";
 import {DocumentEntity} from "../../domain/document-entity";
+import {Constants} from "../common/Constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SectionState {
-  readonly id: number = SectionState.NO_ID
+  readonly id: number = Constants.NO_ID
   readonly title: string = ""
   readonly url: string = ""
   readonly documents: Array<DocumentEntity> = []
   readonly openDocuments: Array<number> = new Array<number>()
   readonly isOpen: boolean = true
-
-  static readonly NO_ID = -1
 }
